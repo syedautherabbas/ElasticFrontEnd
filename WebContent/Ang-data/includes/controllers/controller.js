89bb7d0ff532bd8544b8731e6fdc15c07e548800
syedautherabbas <syedautherabbas@gmail.com>
2016-04-26 03:25:13 +0530

    Some progress on ui router based maps

--- WebContent/Ang-data/includes/controllers/controller.js
+++ WebContent/Ang-data/includes/controllers/controller.js
@@ -1,9 +1,35 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ui.router']);
+
+
+
+
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/home');
+    
+    $stateProvider
+        
+        // HOME STATES AND NESTED VIEWS ========================================
+        .state('home', {
+            url: '/home',
+           templateUrl: 'banner_home.html',
+        controller: 'AppCtrl'
+        })
+        
+        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+        .state('ayatreader', {
+            url: '/surah/:sid',
+           templateUrl: 'ayatreader.html',
+        controller: 'AppCtrl2'   
+        });
+        
+});
 
 
 
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("Hello World from controller");
+    console.log("controller 1");
 
 
 var refresh = function() {
@@ -15,7 +41,7 @@ var refresh = function() {
 //ayat data get  call 
 
   $http.get('http://localhost:8080/Noor-E-Iman/Quran/listofallsurahs').success(function(response) {
-    console.log("GET  REQ FOR AYATOFTHEDAY SUCCESS ");
+    console.log("GET  REQ FOR ALL SURAH SUCCESS ");
 
     
    
@@ -23,6 +49,46 @@ var refresh = function() {
 
      
   });
+
+
+   
+};
+
+
+console.log('CTRL 111111111');
+refresh();
+
+
+
+
+}]);﻿
+
+
+
+myApp.controller('AppCtrl2', ['$scope', '$http','$stateParams', function($scope, $http,$stateParams) {
+    console.log("controller 2"+$stateParams.sid);
+
+
+var refresh = function() {
+
+//username get call
+
+
+
+//ayat data get  call 
+
+  $http.get('http://localhost:8080/Noor-E-Iman/Quran/surah/'+$stateParams.sid+'/list').success(function(response) {
+    console.log("GET  REQ FOR suah 1 list  SUCCESS ");
+
+    
+   
+    $scope.ayatlist = response;
+
+     
+  });
+
+
+
 
 
 
@@ -38,8 +104,13 @@ var refresh = function() {
 
 
 
-console.log('REFRESH EXECUTED!');
+console.log('ctrl2222222222222!');
 refresh();
+
+$scope.displaychapter = function() {
+   console.log("DISPLAY SURAH!! "+$scope.chapter);
+  
+}
 
 
 
