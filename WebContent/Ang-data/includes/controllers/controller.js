174477c113f7f5363a61240b8476fb4d858efc8d
syedautherabbas <syedautherabbas@gmail.com>
2016-04-27 03:18:01 +0530

    Surah_homepage, Ayat reader

--- WebContent/Ang-data/includes/controllers/controller.js
+++ WebContent/Ang-data/includes/controllers/controller.js
@@ -1,123 +1,127 @@
-var myApp = angular.module('myApp', ['ui.router']);
-
-
-
-
+var myApp = angular.module('myApp', [ 'ui.router' ]);
 
 myApp.config(function($stateProvider, $urlRouterProvider) {
-    
-    $urlRouterProvider.otherwise('/home');
-    
-    $stateProvider
-        
-        // HOME STATES AND NESTED VIEWS ========================================
-        .state('home', {
-            url: '/home',
-           templateUrl: 'banner_home.html',
-        controller: 'AppCtrl'
-        })
-        
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
-        .state('ayatreader', {
-            url: '/surah/:sid',
-           templateUrl: 'ayatreader.html',
-        controller: 'AppCtrl2'   
-        });
-        
+
+	$urlRouterProvider.otherwise('/home');
+
+	$stateProvider
+
+	// HOME STATES AND NESTED VIEWS ========================================
+	.state('home', {
+		url : '/home',
+		templateUrl : 'banner_home.html',
+		controller : 'AppCtrl'
+	})
+
+	// ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+	.state('ayatreader', {
+		url : '/surah/:sid',
+		templateUrl : 'ayatreader.html',
+		controller : 'AppCtrl2'
+	});
+
 });
 
+myApp
+		.controller(
+				'AppCtrl',
+				[
+						'$scope',
+						'$http',
+						function($scope, $http) {
+							console.log("controller 1");
 
+							var refresh = function() {
 
-myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
-    console.log("controller 1");
+								// username get call
 
+								// ayat data get call
 
-var refresh = function() {
+								$http
+										.get(
+												'http://localhost:8080/Noor-E-Iman/Quran/listofallsurahs')
+										.success(
+												function(response) {
+													console
+															.log("GET  REQ FOR ALL SURAH SUCCESS ");
 
-//username get call
+													$scope.surahlist = response;
 
+												});
 
+							};
 
-//ayat data get  call 
+							console.log('CTRL 111111111');
+							refresh();
 
-  $http.get('http://localhost:8080/Noor-E-Iman/Quran/listofallsurahs').success(function(response) {
-    console.log("GET  REQ FOR ALL SURAH SUCCESS ");
+							$scope.displaychapter = function() {
 
-    
-   
-    $scope.surahlist = response;
+								console.log($scope.ayat);
 
-     
-  });
+							}
 
+						} ]);
+myApp
+		.controller(
+				'AppCtrl2',
+				[
+						'$scope',
+						'$http',
+						'$stateParams',
+						function($scope, $http, $stateParams) {
 
-   
-};
+							console.log("controller 2" + $stateParams.sid);
 
+//							var url = window.location.href;
+//							var urlArray = url.split('/');
+//							if (urlArray[5] === "surah") {
+//								document.getElementsByClassName("footer_links")[0].style.display = "none";
+//								document.getElementsByTagName("html")[0].style.background = "#ffffff";
+//							} else {
+//								document.getElementsByClassName("footer_links")[0].style.display = "block";
+//								document.getElementsByTagName("html")[0].style.background = "#c0c0c0";
+//							}
 
-console.log('CTRL 111111111');
-refresh();
+							var refresh = function() {
 
+								// username get call
 
-$scope.displaychapter = function() {
+								// ayat data get call
 
-  for (ayat in $scope.ayat)
-   console.log("DISPLAY SURAH!! "+ayat.chapter);
-  
-}
+								$http
+										.get(
+												'http://localhost:8080/Noor-E-Iman/Quran/surah/'
+														+ $stateParams.sid
+														+ '/list')
+										.success(
+												function(response) {
+													console
+															.log("GET  REQ FOR surah 's ayats  SUCCESS ");
 
+													$scope.ayatlist = response;
 
+												});
+								
+								
+								$http
+								.get(
+										'http://localhost:8080/Noor-E-Iman/Quran/surah/'
+												+ $stateParams.sid
+												)
+								.success(
+										function(response) {
+											console
+													.log("GET  REQ FOR surah name success");
 
-}]);﻿
+											$scope.surah = response;
 
+										});
+								
+					
 
+							};
 
-myApp.controller('AppCtrl2', ['$scope', '$http','$stateParams', function($scope, $http,$stateParams) {
-    console.log("controller 2"+$stateParams.sid);
+							console.log('ctrl2222222222222!');
+							refresh();
 
-
-var refresh = function() {
-
-//username get call
-
-
-
-//ayat data get  call 
-
-  $http.get('http://localhost:8080/Noor-E-Iman/Quran/surah/'+$stateParams.sid+'/list').success(function(response) {
-    console.log("GET  REQ FOR suah 1 list  SUCCESS ");
-
-    
-   
-    $scope.ayatlist = response;
-
-     
-  });
-
-
-
-
-
-
-
-
-   
-};
-
-
-  
-
-   
-
-
-
-console.log('ctrl2222222222222!');
-refresh();
-
-
-
-
-
-
-
-}]);﻿
+						} ]);
